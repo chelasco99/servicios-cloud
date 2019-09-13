286c5698ec97e1b6b3966fe5b51d1a3823d4a8e5
Enzo <EnzoGl33@hotmail.com>
2019-09-13 16:52:14 -0300

    getTracks artist

--- visado_1/exports/artist.js
+++ visado_1/exports/artist.js
@@ -24,7 +24,14 @@ class Artista{
     removeAlbum(album){
         this.albums.pop(album)
     }
+
+    getTracks(){
+        let artistTracks = []
+        this.albums.forEach(function(elem){
+            artistTracks.concat(elem.tracks)
+        },tracks)
+    }
 }
 
 
-module.exports = Artista  
+module.exports = Artista
