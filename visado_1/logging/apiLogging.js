001672621bb8a4eb2383edf5776efa5a2dbb0cfc
Nacho <nchelasco@gmail.com>
2019-11-26 13:56:17 -0300

    agregue algunas cosas en logg

--- visado_1/logging/apiLogging.js
+++ visado_1/logging/apiLogging.js
@@ -2,26 +2,39 @@ let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
 let router = express.Router()
+let winston = require('winston')
+let {Loggly} = require('winston-loggly-bulk')
 
 app.use(bodyParser.json())
 app.use('/api',router)
 
-estaActivo = true
+logActivo = true
 
 router.get('/activar', (req, res) => {
-    estaActivo = true
+    logActivo = true
     console.log("Esta activado")
     res.status(200)
     res.json({status: 200, message: "El log esta activo"})
 })
 
 router.get('/desactivar', (req,res) => {
-    estaActivo = false 
+    logActivo = false 
     console.log("Esta desactivado")
     res.status(200)
     res.json({status: 200, message: "El log esta desactivado"})
 })
 
+
+
+//Envia log a loggly
+winston.add(new Loggly({
+    token: "f50778c4-efb5-444f-bf79-e4f89df95ede",
+    subdomain: "nachochelasco",
+    tags: ["Winston-NodeJS"],
+    json: true
+}));
+
+winston.log('info', "Hola Mundo desde Node.js");
 
 
 app.listen(8002, ()=>{
