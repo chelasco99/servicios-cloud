077d313fd58c321ef4247493f172756f60329cb5
Enzo <EnzoGl33@hotmail.com>
2019-11-28 16:54:16 -0300

    change method to verify

--- visado_1/notificaciones/checkArtistUNQFY.js
+++ visado_1/notificaciones/checkArtistUNQFY.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 function verifyArtist(artistId){
     return fetch('http://localhost:8000/api/artists/' + artistId, {
-        method: 'GET', //VERIFICAR QUE EL GET SEA ASI EN LA DOCUMENTACION DE FETCH
+        method: 'GET', 
         headers:{
             'Content-Type': 'application/json'
         }
